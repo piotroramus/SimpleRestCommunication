8b9df2ca5bc32b19e82a2a9fae0dcf2f132cfd2c
piotroramus <piotroramus@mail.com>
2017-05-08 21:54:43 +0200

    Extracted server url host and port to argparse (in client code)

--- client.py
+++ client.py
@@ -1,8 +1,16 @@
+import argparse
 import requests
 
 if __name__ == '__main__':
 
-    url = "http://127.0.0.1:5000/hello"
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', default='127.0.0.1', help='IP address of the listening server')
+    parser.add_argument('--port', default='5000', help='port of the listening server')
+    args = parser.parse_args()
+    host = args.host
+    port = args.port
+
+    url = "http://{}:{}/hello".format(host, port)
 
     try:
         r = requests.get(url)
